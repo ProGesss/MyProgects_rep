644c46737ae629a647d5e93fe08e34e089d9c6d3
Gess Konstantin <konstantingess@gmail.com>
2019-04-15 14:39:41 +0300

    init commnit

---
+++ php_basics_tasks/4.php
@@ -0,0 +1,10 @@
+<?php
+//4. Выведите фразу "Мне %ваш_возраст% лет", например: "Мне 20 лет".
+
+$age = 34;
+
+$name = 'Konstantin';
+
+echo 'My name is ' . $name . 'my age is ' . $age;
+
+?>
---
+++ php_basics_tasks/7.php
@@ -0,0 +1,17 @@
+<?php
+/* 7. Расширьте конструкцию if из п.5-6, выводя фразу:
+ "Вам еще рано работать" при условии, что значение переменной age попадает в диапазон чисел от 0 до 17 (включительно).
+*/
+
+$age = 59;
+
+if ((18 <= $age) && ($age <= 59))
+{
+    echo 'You still work and work';
+}elseif ($age > 59){
+    echo 'You have to retire';
+}elseif ((0 <= $age) && ($age <= 17)){
+    echo 'It is too early for you to work';
+}
+
+?>
